940c852f6a47c73544e5469a230f038e473551fd
Anderson Cafurica <129119434+Emicy963@users.noreply.github.com>
2025-03-21 19:34:27 +0100

    Working in TodoController

---
+++ src/controllers/todo_controller.py
@@ -0,0 +1,20 @@
+from ..models.todo_model import TodoModel
+
+class TodoController:
+    def __init__(self, model: TodoModel):
+        self.model = model
+
+    def handle_get_all_todo(self):
+        return self.model.get_all_todos()
+    
+    def handle_create_todo(self, title, deadline):
+        return self.model.create_todo(title, deadline)
+    
+    def handle_update_todo(self, title, deadline):
+        return self.model.update_todo(title, deadline)
+    
+    def handle_delete_todo(self, todo_id):
+        return self.model.delete_todo(todo_id)
+    
+    def handle_complete_todo(self, todo_id):
+        return self.model.complete_todo(todo_id)
